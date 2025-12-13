abc6b64ed51629adb49ac6dcaebc5b2bf269268d
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-12-13 19:36:33 +0530

    feat(movie): add controller for fetching all movies
    Implement basic controller to handle GET requests for movies collection

---
+++ apps/server/src/controllers/movieController.js
@@ -0,0 +1,10 @@
+import Movie from "../../src/models/Movie.js";
+
+export const getAllMovies = async (req, res) => {
+  try {
+    const movies = await Movie.find({});
+    res.status(200).json(movies);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
