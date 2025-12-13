f23132a63f9a0b5fad8f72640982620359218e1d
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-12-13 19:36:45 +0530

    feat: add movie endpoint and environment config
    - Add GET /movies endpoint using movieController
    - Configure dotenv for environment variables

--- apps/server/src/app.js
+++ apps/server/src/app.js
@@ -1,9 +1,15 @@
 import express from "express";
+import dotenv from "dotenv";
+import { getAllMovies } from "./controllers/movieController.js";
+
+dotenv.config();
 
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+app.get("/movies", getAllMovies);
 
 export default app;
