d1b986515d3b99bd7a11c4ed62e308cd2165ee79
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-12-13 20:19:46 +0530

    feat(movie): add route to get movie by id

--- apps/server/src/routes/movie.route.js
+++ apps/server/src/routes/movie.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAllMovies } from "../controllers/movie.controller.js";
+import { getAllMovies, getMovieById } from "../controllers/movie.controller.js";
 // import { authenticate } from "../middlewares/auth.middleware.js";
 // import { validate } from "../middlewares/validation.middleware.js";
 // import { postSchema } from "../validations/post.validation.js";
@@ -7,5 +7,6 @@ import { getAllMovies } from "../controllers/movie.controller.js";
 const router = express.Router();
 
 router.get("/movies", getAllMovies);
+router.get("/movies/:id", getMovieById);
 
 export default router;
